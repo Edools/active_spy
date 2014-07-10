b5b684be4d96b65bf069855f8aa7f988459a2c29
Douglas Camata <d.camata@gmail.com>
2014-07-10 11:08:51 -0300

    loading the yml configuration file only if it exists

--- config/initializers/active_event_configuration_loader.rb
+++ config/initializers/active_event_configuration_loader.rb
@@ -1,8 +1,13 @@
 require 'hashie'
 
 yml_path = Rails.root.join('config', 'active_spy.yml')
-all_settings = YAML.load_file(yml_path)
-env_settings = Hashie::Mash.new(all_settings[Rails.env])
+if File.exists?(yml_path)
+  all_settings = YAML.load_file(yml_path)
+  env_settings = Hashie::Mash.new(all_settings[Rails.env])
+else
+  # TODO: Add a warning here
+  env_settings = {}
+end
 
 ActiveSpy.configure do |config|
   config.name env_settings['name']
