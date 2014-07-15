fb2ab8057f92eb6f208541e3c9d8d7aa1c60e163
Douglas Camata <d.camata@gmail.com>
2014-07-15 18:09:21 -0300

    added the base notifications controller

--- app/controllers/active_spy/notifications_controller.rb
+++ app/controllers/active_spy/notifications_controller.rb
@@ -1,6 +1,13 @@
 module ActiveSpy
   class NotificationsController < ActionController::Base
     def handle
+      hooks = ActiveSpy::Rails::HookList.hooks
+      current_hook = nil
+      hooks.each do |hook|
+        if hook['post_class'].downcase == params['class']
+          "#{params['class']}Listener".constantize.new.handle(params['event'])
+        end
+      end
     end
   end
 end
