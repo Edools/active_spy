08337311e81d1e0f436080e3b6a38d1a00c89de6
Douglas Camata <d.camata@gmail.com>
2014-07-15 18:23:43 -0300

    forgot the mime type (oh Lord)

--- app/controllers/active_spy/notifications_controller.rb
+++ app/controllers/active_spy/notifications_controller.rb
@@ -1,13 +1,16 @@
 module ActiveSpy
   class NotificationsController < ActionController::Base
+
+
     def handle
+      request.format = 'application/json'
       hooks = ActiveSpy::Rails::HookList.hooks
       result = nil
       hooks.each do |hook|
         if hook['post_class'].downcase == params['class']
           listener = "#{hook['post_class']}Listener".constantize
           result = listener.new.handle(params['event'])
-          respond_with result and return
+          respond_with(result, format: :jsnon) and return
         end
       end
       render nothing: true, status: :not_found
