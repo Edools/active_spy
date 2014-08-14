73919fa39dbc397fde92a8706bf6abf5592cb623
Douglas Camata <d.camata@gmail.com>
2014-08-14 12:34:35 -0300

    big refactoring to notifications controller

--- app/controllers/active_spy/notifications_controller.rb
+++ app/controllers/active_spy/notifications_controller.rb
@@ -9,17 +9,50 @@ module ActiveSpy
       result = nil
       hooks.each do |hook|
         if hook['post_class'].downcase == params['class']
-          listener = "#{hook['post_class']}Listener".constantize
-          result = listener.new.handle(params['event'])
-          if result.errors.present?
-            Rails.logger.warn("[EVENT][#{hook['post_class']}] Error on save #{result}: #{result.errors}")
-            render json: result.errors
-          else
-            render json: result
-          end and return
+           handle_result and return
         end
       end
       render nothing: true, status: :not_found
+    end
+  end
+
+  private
+  def handle_result(hook, params)
+    result = get_result(hook, params)
+    if result.is_a? Array
+      handle_array_result(result, params)
+    else
+      handle_model_result(result, params)
+    end
+  end
+
+
+  def get_result(hook, params)
+    listener = "#{hook['post_class']}Listener".constantize
+    result = listener.new.handle(params['event'])
+  end
+
+  def handle_model_result(result, params)
+    if result.errors.present?
+      Rails.logger.warn("[EVENT][#{hook['post_class']}] Error receiving event #{params}")
+      Rails.logger.warn("[EVENT][#{hook['post_class']}] Listener result: #{result}")
+      Rails.logger.warn("[EVENT][#{hook['post_class']}] Result errors: #{result.errors}")
+      render json: result.errors
+    else
+      render nothing: true
+    end
+  end
+
+  def handle_array_result(result, params)
+    model_with_errors = result.select { |m| m.errors.present? }
+    if model_with_errors.any?
+      Rails.logger.warn("[EVENT][#{hook['post_class']}] Error receiving event #{params}")
+      Rails.logger.warn("[EVENT][#{hook['post_class']}] Listener result: #{result}")
+      model_with_errors.each do |model|
+        Rails.logger.warn("[EVENT][#{hook['post_class']}] #{model} errors: #{model.errors}")
+      end
+    else
+      render nothing: true
     end
   end
 end
