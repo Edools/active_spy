5592b06c6b7ebe6decccbe0b44ed7c943f55c372
Douglas Camata <d.camata@gmail.com>
2014-10-01 10:26:06 -0300

    adding more logging to debug awkward errors

--- lib/active_spy/rails/base.rb
+++ lib/active_spy/rails/base.rb
@@ -66,6 +66,10 @@ module ActiveSpy
       def send_event_request
         host = ActiveSpy::Configuration.event_host
         port = ActiveSpy::Configuration.event_port
+        ::Rails.logger.info("[SPY] #{@even_json}")
+        ::Rails.logger.info("[SPY] #{@object.inspect}")
+        ::Rails.logger.info("[SPY] #{@object.instance_variable_get('@actor')}")
+        ::Rails.logger.info("[SPY] #{@object.instance_variable_get('@realm')}")
         begin
           response = RestClient.post "#{host}:#{port}/events", @event_json,
             content_type: :json
